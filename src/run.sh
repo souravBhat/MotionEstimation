85fe44ed3da0c46b066bd0672c619e7ea0e7b351
SOURAV BHATTACHARJEE <sourav@connect.hku.hk>
2020-11-23 21:43:37 -0500

    update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ../bin
 # Run on cpu.
-gcc block.c prediction_frame.c main.c utils.c -o ../bin/mes && ../bin/mes ../frames/YF1.yuv ../frames/YF2.yuv
+gcc block.c prediction_frame.c main.c utils.c -o ../bin/mes && ../bin/mes ../frames/YF1.yuv ../frames/YF2.yuv ../results/cpu/race
